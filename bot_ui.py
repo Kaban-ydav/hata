b34b8aef32ed4e87f005a33dd251f206fa139b12
Davyd Mazanko <sekretqwy@gmail.com>
2026-08-01 16:10:28 +0100

    fix playwright stealth import

--- bot_ui.py
+++ bot_ui.py
@@ -14,7 +14,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 import sys
-from playwright_stealth import stealth_async
+from playwright_stealth import stealth
 # === НАСТРОЙКИ ===
 TOKEN = "8758634330:AAEtOTqGStH5QH5jWowfAk70k127-oDy6Lw"
 
@@ -666,7 +666,7 @@ async def sites_parser_loop():
                 context = await p.chromium.launch_persistent_context(**launch_options)
 
                 page = context.pages[0] if context.pages else await context.new_page()
-                await stealth_async(page)  # 👈 Вот сюда вставляем!
+                await stealth(page)  # 👈 Замени stealth_async на stealth (или просто stealth(page))
                 await page.add_init_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 
                 while True:
